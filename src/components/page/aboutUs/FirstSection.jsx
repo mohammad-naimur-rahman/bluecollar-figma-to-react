22aec58cdcb41870099d2899de593cb231a06e98
Naimur <naeemhasan28@gmail.com>
2023-03-29 23:50:07 +0600

    added some components

--- src/components/page/aboutUs/FirstSection.jsx
+++ src/components/page/aboutUs/FirstSection.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Heading from '../../ui/Heading'
+import icon from '@assets/images/homepage/icon.png'
+import hero from '@assets/images/about-us/hero.png'
 
 const FirstSection = () => {
   return (
-    <section className="bg-white">
-      <h1>Hello</h1>
+    <section className="bg-white p-xxl">
+      <div className="container">
+        <div className="flex">
+          <div className="w-[60%]">
+            <Heading title="Welcome to BlueCollar" img={icon} />
+            <h2 className="text-secondary">We're commited to Quality</h2>
+            <p className="text-gray">
+              Proactively envisined multimeda based exptis and cross media growth strategies seamlessly visualized
+              quality a intellectual capitals withouts superiors collaboration ideas sharing holistically pontificated
+              installed base portals after maintainable products.
+            </p>
+          </div>
+          <div className="w-[40%]">
+            <img src={hero} alt="hero image" />
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
