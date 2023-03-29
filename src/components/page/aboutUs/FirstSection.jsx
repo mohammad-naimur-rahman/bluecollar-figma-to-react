140370d412ef1dab7da339e1d87959b246620fa4
Naimur <naeemhasan28@gmail.com>
2023-03-29 23:35:05 +0600

    started work for about us page

---
+++ src/components/page/aboutUs/FirstSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirstSection = () => {
+  return (
+    <section className="bg-white">
+      <h1>Hello</h1>
+    </section>
+  )
+}
+
+export default FirstSection
--- src/page/AboutUs.tsx
+++ src/page/AboutUs.tsx
@@ -1,11 +1,13 @@
 import Layout from '@components/ui/Layout.jsx'
 import BreadCrumb from '@components/ui/BreadCrumb.jsx'
 import bg from '@assets/images/about-us/bg.png'
+import FirstSection from '@components/page/aboutUs/FirstSection.jsx'
 
 const AboutUs = () => {
   return (
     <Layout title="About Us">
       <BreadCrumb title="About Us" img={bg} />
+      <FirstSection />
     </Layout>
   )
 }
