73c4f410049d97ee3a3ae45f593c0a876e975a04
Naimur <naeemhasan28@gmail.com>
2023-04-01 12:06:23 +0600

    fixed breadcrumb z-index issue and exposed host for dev env

--- src/components/ui/BreadCrumb.jsx
+++ src/components/ui/BreadCrumb.jsx
@@ -4,7 +4,7 @@ import classNames from 'classnames'
 
 const BreadCrumb = ({ title, img }) => {
   return (
-    <section className="w-full h-[435px] relative">
+    <section className="w-full h-[435px] relative -z-10">
       <img src={img} alt={title} className="absolute inset-0 -z-10 object-cover w-full h-full blur-[15px]" />
       <div className={classNames(styles.breadCrumb, 'absolute inset-0 w-full h-full -z-10')}></div>
       <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-white">{title}</h1>
