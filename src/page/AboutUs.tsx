ed3cde3a207f014ada0342f858e66c67c23403b4
Naimur <naeemhasan28@gmail.com>
2023-04-02 12:55:14 +0600

    added two sections in the services page

--- src/page/AboutUs.tsx
+++ src/page/AboutUs.tsx
@@ -1,9 +1,9 @@
 import Layout from '@components/ui/Layout.jsx'
 import BreadCrumb from '@components/ui/BreadCrumb.jsx'
 import bg from '@assets/images/about-us/bg.png'
-import FirstSection from '@components/page/aboutUs/FirstSection.jsx'
-import SecondSection from '@components/page/aboutUs/SecondSection.jsx'
-import ThirdSection from '@components/page/aboutUs/ThirdSection.jsx'
+import FirstSection from '@components/page/AboutUs/FirstSection.jsx'
+import SecondSection from '@components/page/AboutUs/SecondSection.jsx'
+import ThirdSection from '@components/page/AboutUs/ThirdSection.jsx'
 
 const AboutUs = () => {
   return (
--- src/page/Services.tsx
+++ src/page/Services.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import Layout from '@components/ui/Layout.jsx'
+import BreadCrumb from '@components/ui/BreadCrumb.jsx'
+import breadcrumbImg from '@assets/images/services/breadcrumb.png'
+import MainServices from '@components/page/Services/MainServices.jsx'
+import OurServices from '@components/page/HomePage/OurServices.jsx'
 
 const Services = () => {
-  return <div>Services</div>
+  return (
+    <Layout title="Services">
+      <BreadCrumb title="Services" img={breadcrumbImg} />
+      <MainServices />
+      <OurServices />
+    </Layout>
+  )
 }
 
 export default Services
