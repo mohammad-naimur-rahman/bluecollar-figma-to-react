1b475d248c02b8c9d767385c1d41b40838adbfdc
Naimur <naeemhasan28@gmail.com>
2023-04-08 02:54:47 +0600

    fix: breadcrumb title text center issue

--- src/components/ui/BreadCrumb.jsx
+++ src/components/ui/BreadCrumb.jsx
@@ -7,7 +7,7 @@ const BreadCrumb = ({ title, img }) => {
     <section className="w-full h-[435px] lg:h-96 md:h-60 relative -z-10">
       <img src={img} alt={title} className="absolute inset-0 -z-10 object-cover w-full h-full blur-[15px]" />
       <div className={classNames(styles.breadCrumb, 'absolute inset-0 w-full h-full -z-10')}></div>
-      <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-white min-w-[max-content]">
+      <h1 className="absolute w-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 text-white text-center">
         {title}
       </h1>
     </section>
