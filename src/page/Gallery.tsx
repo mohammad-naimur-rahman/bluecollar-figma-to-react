46c415afbcb1df7d9d48fcf83311b8a3463703ec
Naimur <naeemhasan28@gmail.com>
2023-04-10 23:24:45 +0600

    completed gallery with mobile responsive

--- src/page/Gallery.tsx
+++ src/page/Gallery.tsx
@@ -11,7 +11,7 @@ import BreadCrumb from '@components/ui/BreadCrumb'
 const GalleryCard = ({ img, title, description }: { img: string; title: string; description: string }) => {
   return (
     <div className="p-4 xl:p-2 sm:p-0 sm:pb-3 w-1/3 lg:w-1/2 sm:w-full text-center cursor-pointer">
-      <div className="p-5 xl:p-2 lg:p-4 xsm:p-2 bg-white h-full">
+      <div className="p-5 xl:p-2 lg:p-4 xsm:p-2 bg-white h-full border-[1px] border-gray border-opacity-30">
         <img src={img} alt={title} className="w-full h-auto object-cover" />
         <h6 className="text-secondary pt-4 pb-1">{title}</h6>
         <p className="text-gray">{description}</p>
@@ -24,14 +24,16 @@ const Gallery = () => {
   return (
     <Layout title="Gallary">
       <BreadCrumb img={bg} title="Our Projects" />
-      <div className="container flex flex-wrap justify-around p-xl">
-        <GalleryCard title="Electrical & Maintenance" description="House, office" img={image1} />
-        <GalleryCard title="Industry Machine Issue" description="House, Office" img={image2} />
-        <GalleryCard title="Car Repai & Maintenance" description="Car, Bike, Load vehicle" img={image3} />
-        <GalleryCard title="Interior/Exterior Services" description="Installation" img={image4} />
-        <GalleryCard title="Building Installation" description="House, office" img={image5} />
-        <GalleryCard title="Electrical Installation" description="House, office" img={image6} />
-      </div>
+      <section className="bg-white">
+        <div className="container flex flex-wrap justify-around p-xl">
+          <GalleryCard title="Electrical & Maintenance" description="House, office" img={image1} />
+          <GalleryCard title="Industry Machine Issue" description="House, Office" img={image2} />
+          <GalleryCard title="Car Repai & Maintenance" description="Car, Bike, Load vehicle" img={image3} />
+          <GalleryCard title="Interior/Exterior Services" description="Installation" img={image4} />
+          <GalleryCard title="Building Installation" description="House, office" img={image5} />
+          <GalleryCard title="Electrical Installation" description="House, office" img={image6} />
+        </div>
+      </section>
     </Layout>
   )
 }
